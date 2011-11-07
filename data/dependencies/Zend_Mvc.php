125e3b4f179ff5891d7f07c3f3ec27e6edfe3150
Intiilapa <intiilapa-github@ssji.net>
2011-11-07 22:28:45 +0100

    Fixes missing components data (and dependencies)
    The scan tool doesn't include some components with (hard) dependencies. Two test cases:
      - Mvc
      - View (without helpers)

---
+++ data/dependencies/Zend_Mvc.php
@@ -0,0 +1,12 @@
+<?php return array (
+  'optional' => 
+  array (
+    'Zend_Di',
+    'Zend_EventManager',
+    'Zend_Loader',
+    'Zend_Http',
+    'Zend_Module',
+    'Zend_Uri',
+    'Zend_Stdlib',
+  ),
+);
---
+++ data/dependencies/Zend_View.php
@@ -0,0 +1,11 @@
+<?php return array (
+  'required' => 
+  array (
+    'Zend_Loader',
+    'Zend_Stdlib',
+  ),
+  'optional' => 
+  array (
+    'Zend_Filter',
+  ),
+);
