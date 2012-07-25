b83ee0152d9e3d1deef94707283ad0513421bc28
Matthew Weier O'Phinney <matthew@zend.com>
2012-07-25 17:39:46 -0500

    Fixed pyrus release scripts for external packages
    - it now merges core and external packages to create the list of what to build
    - fixed the regex in release-package.php to be more liberal. Actually, simply
      removed it and now look for a non-empty argv[1]

--- scripts/release-package.php
+++ scripts/release-package.php
@@ -4,11 +4,11 @@
 define('ROOT', realpath(__DIR__ . '/../'));
 
 // check that we have a package name
-if (!isset($_SERVER['argv'][1]) || !preg_match('#\w+_\w+#', $_SERVER['argv'][1])) {
+if (!isset($_SERVER['argv'][1])) {
     script_exit(__FILE__ . ' expects a package name as the only argument');
-} else {
-    $package_name = $_SERVER['argv'][1];
 }
+
+$package_name = $_SERVER['argv'][1];
 
 // did you copy config.ini.orig to the proper place?
 if (!file_exists(ROOT . '/data/packager.ini')) {
