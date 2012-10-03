e4dd2a3bc6d3de23424ce0a6209dd02c76091b6f
Matthew Weier O'Phinney <matthew@zend.com>
2012-10-03 13:10:15 -0500

    Use UTC for composer times

--- bin/process_queue.php
+++ bin/process_queue.php
@@ -1,7 +1,7 @@
 #!/usr/local/bin/php
 <?php
 ini_set('memory_limit', -1);
-date_default_timezone_set('America/Los_Angeles');
+date_default_timezone_set('UTC');
 require_once '/var/www/website/vendor/autoload.php';
 $git = '/usr/local/git-1.7.10.2/bin/git';
 
